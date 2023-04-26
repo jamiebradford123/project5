34f26780bf2a3108a25fd7de01c256c709a85cf4
jamiebradford123 <jamiebradford123@hotmail.com>
2023-04-26 16:44:12 +0000

    add src folder

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -14,7 +14,7 @@ def plot_predictions_probabilities(pred_proba, pred_class):
 
     prob_per_class = pd.DataFrame(
         data=[0, 0],
-        index={'Parasitised': 0, 'Uninfected': 1}.keys(),
+        index={'healthy': 0, 'powdery_mildew': 1}.keys(),
         columns=['Probability']
     )
     prob_per_class.loc[pred_class] = pred_proba
@@ -49,17 +49,17 @@ def load_model_and_predict(my_image, version):
     Load and perform ML prediction over live images
     """
 
-    model = load_model(f"outputs/{version}/malaria_detector_model.h5")
+    model = load_model(f"outputs/{version}/powdery-detector.h5")
 
     pred_proba = model.predict(my_image)[0, 0]
 
-    target_map = {v: k for k, v in {'Parasitised': 0, 'Uninfected': 1}.items()}
+    target_map = {v: k for k, v in {'healthy': 0, 'powdery mildew': 1}.items()}
     pred_class = target_map[pred_proba > 0.5]
     if pred_class == target_map[0]:
         pred_proba = 1 - pred_proba
 
     st.write(
-        f"The predictive analysis indicates the sample cell is "
-        f"**{pred_class.lower()}** with malaria.")
+        f"The predictive analysis indicates the sample leaf is "
+        f"**{pred_class.lower()}** with powdery mildew.")
 
     return pred_proba, pred_class
